5110121ad7737b2d8cbb99b14e31dd83f30cda21
Priyanshu-singh11 <ajitsinghsombansi1984@gmail.com>
2026-07-25 04:29:54 +0000

    Remove PKBD-SCHOOL files from root, keep only inside PKBD-SCHOOL folder

--- src/Error.jsx
+++
@@ -1,9 +0,0 @@
-import './App.css'
-import React from 'react'
-export default function Error(){
-  return(
-    <>
-      <p className="error">404<span className="notfound mx-2">|</span>not found</p>
-    </>
-    )
-}
--- src/Send.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './Contact.css'
-const Send = () => {
-  return(
-    <h1 className="sending">Sending...</h1>
-    )
-}
-export default Send;
